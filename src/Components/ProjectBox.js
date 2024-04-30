c9122531809c5291dbf2c6f3bfeef1d50bcc28d7
Divy <divyprajapati027@gmail.com>
2024-04-30 22:10:04 +0530

    improved project description

--- src/Components/ProjectBox.js
+++ src/Components/ProjectBox.js
@@ -5,19 +5,19 @@ import {CgFileDocument} from "react-icons/cg";
 
 const  ProjectBox = ({projectPhoto, projectName}) => {
   const desc = {
-    Project1Desc : "This is a staking DApp designed for the Basepunk NFT collection on BaseChain. In addition to developing the staking contract with custom reward logic and integrating it into the frontend, I have also created the UI for this project.",
+    Project1Desc : "This is a staking DApp tailored for the Basepunk NFT collection on BaseChain. I crafted a sophisticated staking contract with custom reward mechanisms, seamlessly integrated it into the frontend, and meticulously designed the user interface to enhance user experience and engagement.",
     Project1Website : "https://stake.basepunk.xyz/",
 
-    Project2Desc : "This is a migration portal for 4DOTHq. I have developed a smart contract that facilitates migration for a limited time period, along with integrating it into the frontend and creating the frontend UI.",
+    Project2Desc : "This is the 4DOTHq Migration Portal, designed to streamline token migration within a limited timeframe. I developed a robust smart contract for secure and efficient migration, seamlessly integrated it into the frontend, and optimized Migration data storage using MongoDB Atlas for enhanced performance.",
     Project2Website : "https://www.4dot.io/",
 
-    Project3Desc:"This is a dApp designed for migrating ASAP Sniper bot v1 tokens to v2 tokens. In addition to developing the smart contract, I have integrated it into the frontend using Wagmi and implemented the WalletConnect model for Web3 connection.",
+    Project3Desc:"This dApp streamlines the migration process from ASAP Sniper bot v1 tokens to v2 tokens. I engineered the smart contract and seamlessly integrated it into the frontend using Wagmi. To ensure smooth Web3 connection, I implemented the WalletConnect model.",
     Project3Website:"https://www.asapbot.xyz/",
     
     Project4Desc:"This is the BitBrawl airdrop platform. I integrated the Twitter API, implemented mission and referral logic, and addressed several bugs. Furthermore, as a part of team we have enhanced various functionalities in both the frontend and backend to meet the client's requirements.",
     Project4Website:"https://airdrop.bitbrawl.io/",
 
-    Project5Desc:"This is the BasedHub TokenLaunchpad by 2x Solutions. I developed this dApp to enable users to create custom ERC20 tokens, conduct airdrops, and create liquidity pools directly from the frontend. I designed the smart contract to automate these processes for the user, simplifying token creation and management.",
+    Project5Desc:"This is the BasedHub TokenLaunchpad. It empowers users to effortlessly deploy custom ERC20 tokens across multiple blockchains. With a focus on user convenience, I engineered the smart contract to automate token deployment, airdrops, and liquidity pool creation directly from the frontend, streamlining the token creation and management process..",
     Project5Website:"https://token-launchpad-fe.vercel.app/launchpad",
 
     Project6Desc:"This is the Telegram Stars Arena Sniper Bot, designed to enable users to automatically snipe keys of Star Arena users directly from Telegram.",
