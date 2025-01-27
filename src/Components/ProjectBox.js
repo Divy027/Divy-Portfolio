1f2a23019f76b2883937179039fcfc718fdedfa8
div-lync <divy@lync.world>
2025-01-27 10:33:53 +0530

    update

--- src/Components/ProjectBox.js
+++ src/Components/ProjectBox.js
@@ -5,25 +5,29 @@ import {CgFileDocument} from "react-icons/cg";
 
 const  ProjectBox = ({projectPhoto, projectName}) => {
   const desc = {
-    Project1Desc : "This is a staking DApp tailored for the Basepunk NFT collection on BaseChain. I crafted a sophisticated staking contract with custom reward mechanisms, seamlessly integrated it into the frontend, and meticulously designed the user interface to enhance user experience and engagement.",
-    Project1Website : "https://stake.basepunk.xyz/",
+    Project1Desc : "This is a Introductory Websited for AI agent on solana blockchain called Ayka-Holywhore and it's made using Next.js ",
+    Project1Website : "https://ayka-landing.vercel.app/",
 
-    Project2Desc : "This is the 4DOTHq Migration Portal, designed to streamline token migration within a limited timeframe. I developed a robust smart contract for secure and efficient migration, seamlessly integrated it into the frontend, and optimized Migration data storage using MongoDB Atlas for enhanced performance.",
-    Project2Website : "https://www.4dot.io/",
+    Project2Desc : "This is a staking DApp tailored for the Basepunk NFT collection on BaseChain. I crafted a sophisticated staking contract with custom reward mechanisms, seamlessly integrated it into the frontend, and meticulously designed the user interface to enhance user experience and engagement.",
+    Project2Website : "https://stake.basepunk.xyz/",
 
-    Project3Desc:"This dApp streamlines the migration process from ASAP Sniper bot v1 tokens to v2 tokens. I engineered the smart contract and seamlessly integrated it into the frontend using Wagmi. To ensure smooth Web3 connection, I implemented the WalletConnect model.",
-    Project3Website:"https://www.asapbot.xyz/",
+
+    Project3Desc : "This is the 4DOTHq Migration Portal, designed to streamline token migration within a limited timeframe. I developed a robust smart contract for secure and efficient migration, seamlessly integrated it into the frontend, and optimized Migration data storage using MongoDB Atlas for enhanced performance.",
+    Project3Website : "https://www.4dot.io/",
+
+    Project4Desc:"This dApp streamlines the migration process from ASAP Sniper bot v1 tokens to v2 tokens. I engineered the smart contract and seamlessly integrated it into the frontend using Wagmi. To ensure smooth Web3 connection, I implemented the WalletConnect model.",
+    Project4Website:"https://www.asapbot.xyz/",
     
-    Project4Desc:"This is the BitBrawl airdrop platform. I integrated the Twitter API, implemented mission and referral logic, and addressed several bugs. Furthermore, as a part of team we have enhanced various functionalities in both the frontend and backend to meet the client's requirements.",
-    Project4Website:"https://airdrop.bitbrawl.io/",
+    Project5Desc:"This is the BitBrawl airdrop platform. I integrated the Twitter API, implemented mission and referral logic, and addressed several bugs. Furthermore, as a part of team we have enhanced various functionalities in both the frontend and backend to meet the client's requirements.",
+    Project5Website:"https://airdrop.bitbrawl.io/",
 
-    Project5Desc:"This is the BasedHub TokenLaunchpad. It empowers users to effortlessly deploy custom ERC20 tokens across multiple blockchains. With a focus on user convenience, I engineered the smart contract to automate token deployment, airdrops, and liquidity pool creation directly from the frontend, streamlining the token creation and management process..",
-    Project5Website:"https://token-launchpad-fe.vercel.app/launchpad",
+    Project6Desc:"This is the BasedHub TokenLaunchpad. It empowers users to effortlessly deploy custom ERC20 tokens across multiple blockchains. With a focus on user convenience, I engineered the smart contract to automate token deployment, airdrops, and liquidity pool creation directly from the frontend, streamlining the token creation and management process..",
+    Project6Website:"https://token-launchpad-fe.vercel.app/launchpad",
 
-    Project6Desc:"This is the Telegram Stars Arena Sniper Bot, designed to enable users to automatically snipe keys of Star Arena users directly from Telegram.",
+    Project7Desc:"This is the Telegram Stars Arena Sniper Bot, designed to enable users to automatically snipe keys of Star Arena users directly from Telegram.",
 
-    Project7Desc: "This is the Jew Coin website on BaseChain, featuring a comprehensive suite of functionalities. I've developed an ERC20 token, implemented a staking smart contract, and curated an NFT collection. Additionally, I've integrated metadata for enhanced token representation and utility.",
-    Project7Website: "https://jewcoin.io/"
+    Project8Desc: "This is the Jew Coin website on BaseChain, featuring a comprehensive suite of functionalities. I've developed an ERC20 token, implemented a staking smart contract, and curated an NFT collection. Additionally, I've integrated metadata for enhanced token representation and utility.",
+    Project8Website: "https://jewcoin.io/"
 
 
 
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -4,13 +4,14 @@ import NewsletterImage from '../images/NewsletterImage.png';
 import RogfreeImage from '../images/RogfreeImage.png';
 import TindogImage from '../images/TindogImage.png';
 import WigglesImage from '../images/WigglesImage.png';
-import Pro1 from "../images/Project1.png";
-import Pro2 from "../images/Project2.png";
-import Pro3 from "../images/Project3.png";
-import Pro4 from "../images/Project4.png";
-import Pro5 from "../images/Project5.png";
-import Pro6 from "../images/Project6.png";
-import Pro7 from "../images/Project7.png";
+import Pro1 from "../images/AYKA.png"
+import Pro2 from "../images/Project1.png";
+import Pro3 from "../images/Project2.png";
+import Pro4 from "../images/Project3.png";
+import Pro5 from "../images/Project4.png";
+import Pro6 from "../images/Project5.png";
+import Pro7 from "../images/Project6.png";
+import Pro8 from "../images/Project7.png";
 
 const Projects = () => {
   return (
@@ -22,8 +23,9 @@ const Projects = () => {
         <ProjectBox projectPhoto={Pro3} projectName="Project3" />
         <ProjectBox projectPhoto={Pro4} projectName="Project4" />
         <ProjectBox projectPhoto={Pro5} projectName="Project5" />
-        <ProjectBox projectPhoto={Pro7} projectName="Project7" />
         <ProjectBox projectPhoto={Pro6} projectName="Project6" />
+        <ProjectBox projectPhoto={Pro7} projectName="Project7" />
+        <ProjectBox projectPhoto={Pro8} projectName="Project8" />
       </div>
 
     </div>
